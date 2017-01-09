a38aba5bb5c7d3e3e5a74615a8a9d735abf7af8a
Casak <casak123@gmail.com>
2017-01-10 01:02:18 +0200

    Add Callback to base interactor

--- app/src/main/java/casak/ru/geofencer/domain/interactors/base/Interactor.java
+++ app/src/main/java/casak/ru/geofencer/domain/interactors/base/Interactor.java
@@ -6,6 +6,12 @@ package casak.ru.geofencer.domain.interactors.base;
  */
 public interface Interactor {
 
+    interface Callback{
+        void onStart();
+        void onFinish();
+        void onError();
+    }
+
     /**
      * This is the main method that starts an interactor. It will make sure that the interactor operation is done on a
      * background thread.
