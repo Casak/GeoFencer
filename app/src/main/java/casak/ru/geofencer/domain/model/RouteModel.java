c8c045b8633527d833688dbc558d780126d62065
Casak <casak123@gmail.com>
2017-01-10 01:05:40 +0200

    Fix NullPointer when trying to get an empty type

--- app/src/main/java/casak/ru/geofencer/domain/model/RouteModel.java
+++ app/src/main/java/casak/ru/geofencer/domain/model/RouteModel.java
@@ -1,26 +1,32 @@
 package casak.ru.geofencer.domain.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RouteModel {
-    private int id;
-
-    public RouteModel(int id){
-        this.id = id;
-    }
-
-    private List<Point> routePoints;
-    private Type routeType;
-
     public enum Type {
         FROM_BASE,
         FIELD_BUILDING,
         COMPUTED,
         CULTIVATED
     }
+    private int id;
+    private Type type;
+    private List<Point> routePoints;
+    private Type routeType;
+
+    public RouteModel(int id, Type type){
+        this.id = id;
+        this.type = type;
+        routePoints = new ArrayList<>();
+    }
 
     public int getId() {
         return id;
+    }
+
+    public Type getType() {
+        return type;
     }
 
     public List<Point> getRoutePoints() {
@@ -29,5 +35,9 @@ public class RouteModel {
 
     public void setRoutePoints(List<Point> routePoints) {
         this.routePoints = routePoints;
+    }
+
+    public void addRoutePoint(Point newPoint){
+        routePoints.add(newPoint);
     }
 }
