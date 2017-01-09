38713e198b4ab1b933bf9226dcf3ed28dacdd4fc
Casak <casak123@gmail.com>
2017-01-10 00:48:55 +0200

    Dummy ArrowRepository implementation

---
+++ app/src/main/java/casak/ru/geofencer/domain/repository/impl/ArrowRepositoryImpl.java
@@ -0,0 +1,19 @@
+package casak.ru.geofencer.domain.repository.impl;
+
+import casak.ru.geofencer.domain.model.ArrowModel;
+import casak.ru.geofencer.domain.repository.ArrowRepository;
+
+/**
+ * Created on 09.01.2017.
+ */
+public class ArrowRepositoryImpl implements ArrowRepository {
+    @Override
+    public ArrowModel getArrow(ArrowModel.Type type) {
+        return null;
+    }
+
+    @Override
+    public ArrowModel getArrow(Integer id) {
+        return null;
+    }
+}
