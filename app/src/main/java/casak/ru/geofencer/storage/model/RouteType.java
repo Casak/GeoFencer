0bbe127a13b1e22acfd6b9ba9e836a98586a5be2
Casak <casak123@gmail.com>
2017-02-02 12:19:16 +0200

    Fix extending from BaseModel for RouteType

--- app/src/main/java/casak/ru/geofencer/storage/model/RouteType.java
+++ app/src/main/java/casak/ru/geofencer/storage/model/RouteType.java
@@ -3,6 +3,7 @@ package casak.ru.geofencer.storage.model;
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
 import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
 
 import casak.ru.geofencer.storage.db.GeoDatabase;
 
@@ -11,7 +12,7 @@ import casak.ru.geofencer.storage.db.GeoDatabase;
  */
 
 @Table(database = GeoDatabase.class)
-public class RouteType {
+public class RouteType extends BaseModel {
     @Column
     @PrimaryKey
     int id;
