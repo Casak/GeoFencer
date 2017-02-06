4117dc9f208041acabb11bd6908f0247028dec10
Casak <casak123@gmail.com>
2017-02-06 17:04:42 +0200

    Change LocationRepository API

--- app/src/main/java/casak/ru/geofencer/domain/repository/LocationRepository.java
+++ app/src/main/java/casak/ru/geofencer/domain/repository/LocationRepository.java
@@ -3,12 +3,8 @@ package casak.ru.geofencer.domain.repository;
 import casak.ru.geofencer.domain.model.Point;
 
 public interface LocationRepository {
-    public enum Destination{
-        COORDS_TABLE,
-        ROUTE_TABLE
-    }
 
-    boolean insert(Point point, Destination destination);
+    boolean insert(Point point);
 
     boolean update(Point point);
 
