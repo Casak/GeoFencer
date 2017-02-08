44aad78b16d72fcd417243a33671880310751c65
Casak <casak123@gmail.com>
2017-02-08 18:37:00 +0200

    (refactor) Delete singleton implementation from MainTread and threadExecutor (dagger will keep singletons)

--- app/src/main/java/casak/ru/geofencer/domain/executor/impl/ThreadExecutor.java
+++ app/src/main/java/casak/ru/geofencer/domain/executor/impl/ThreadExecutor.java
@@ -14,18 +14,15 @@ import java.util.concurrent.TimeUnit;
  */
 public class ThreadExecutor implements Executor {
 
-    // This is a singleton
-    private static volatile ThreadExecutor sThreadExecutor;
-
     private static final int                     CORE_POOL_SIZE  = 3;
     private static final int                     MAX_POOL_SIZE   = 5;
     private static final int                     KEEP_ALIVE_TIME = 120;
     private static final TimeUnit                TIME_UNIT       = TimeUnit.SECONDS;
-    private static final BlockingQueue<Runnable> WORK_QUEUE      = new LinkedBlockingQueue<Runnable>();
+    private static final BlockingQueue<Runnable> WORK_QUEUE      = new LinkedBlockingQueue<>();
 
     private ThreadPoolExecutor mThreadPoolExecutor;
 
-    private ThreadExecutor() {
+    public ThreadExecutor() {
         long keepAlive = KEEP_ALIVE_TIME;
         mThreadPoolExecutor = new ThreadPoolExecutor(
                 CORE_POOL_SIZE,
@@ -47,17 +44,5 @@ public class ThreadExecutor implements Executor {
                 interactor.onFinished();
             }
         });
-    }
-
-    /**
-     * Returns a singleton instance of this executor. If the executor is not initialized then it initializes it and returns
-     * the instance.
-     */
-    public static Executor getInstance() {
-        if (sThreadExecutor == null) {
-            sThreadExecutor = new ThreadExecutor();
-        }
-
-        return sThreadExecutor;
     }
 }
--- app/src/main/java/casak/ru/geofencer/threading/MainThreadImpl.java
+++ app/src/main/java/casak/ru/geofencer/threading/MainThreadImpl.java
@@ -10,24 +10,14 @@ import casak.ru.geofencer.domain.executor.MainThread;
  */
 public class MainThreadImpl implements MainThread {
 
-    private static MainThread sMainThread;
-
     private Handler mHandler;
 
-    private MainThreadImpl() {
+    public MainThreadImpl() {
         mHandler = new Handler(Looper.getMainLooper());
     }
 
     @Override
     public void post(Runnable runnable) {
         mHandler.post(runnable);
-    }
-
-    public static MainThread getInstance() {
-        if (sMainThread == null) {
-            sMainThread = new MainThreadImpl();
-        }
-
-        return sMainThread;
     }
 }
