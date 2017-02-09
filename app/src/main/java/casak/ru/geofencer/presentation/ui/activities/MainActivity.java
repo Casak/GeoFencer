ed00fa9dbf459e6c468337d15d5e06fc7657dc53
Casak <casak123@gmail.com>
2017-02-09 10:45:32 +0200

    Switch UI to the new MainActivity

---
+++ app/src/main/java/casak/ru/geofencer/presentation/ui/activities/MainActivity.java
@@ -0,0 +1,21 @@
+package casak.ru.geofencer.presentation.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import casak.ru.geofencer.R;
+
+
+/**
+ * Created on 08.02.2017.
+ */
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
