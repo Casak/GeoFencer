8b92357b79a8db46e005d87f6f437432a4ee0096
Casak <casak123@gmail.com>
2017-02-09 10:45:58 +0200

    Create BaseActivity and BaseView

---
+++ app/src/main/java/casak/ru/geofencer/presentation/ui/BaseView.java
@@ -0,0 +1,8 @@
+package casak.ru.geofencer.presentation.ui;
+
+/**
+ * Created on 09.02.2017.
+ */
+
+public interface BaseView {
+}
---
+++ app/src/main/java/casak/ru/geofencer/presentation/ui/activities/BaseActivity.java
@@ -0,0 +1,11 @@
+package casak.ru.geofencer.presentation.ui.activities;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created on 08.02.2017.
+ */
+
+public class BaseActivity extends FragmentActivity {
+
+}
