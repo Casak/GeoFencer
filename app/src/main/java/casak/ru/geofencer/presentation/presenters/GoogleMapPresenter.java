96ff7f122d95597afb5821904b01799acc3736fe
Casak <casak123@gmail.com>
2017-02-10 17:33:24 +0200

    Extends GoogleMapPresenter with showPolygon()

--- app/src/main/java/casak/ru/geofencer/presentation/presenters/GoogleMapPresenter.java
+++ app/src/main/java/casak/ru/geofencer/presentation/presenters/GoogleMapPresenter.java
@@ -2,24 +2,30 @@ package casak.ru.geofencer.presentation.presenters;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import casak.ru.geofencer.domain.interactors.CreateFieldInteractor;
 import casak.ru.geofencer.presentation.presenters.base.BasePresenter;
 
 /**
  * Created on 09.02.2017.
  */
 
-public interface GoogleMapPresenter extends BasePresenter, OnMapReadyCallback {
-    interface View{
+public interface GoogleMapPresenter extends BasePresenter, CreateFieldInteractor.Callback, OnMapReadyCallback {
+    interface View {
         void setMap(GoogleMap googleMap);
+
         Polyline showPolyline(PolylineOptions options);
 
-
+        Polygon showPolygon(PolygonOptions options);
     }
 
     boolean isFieldBuilding();
+
     void startBuildField();
+
     void finishBuildField();
 }
