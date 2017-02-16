283aa64d0ba0f9ee43c4eb99aea2722a98751121
Casak <casak123@gmail.com>
2017-02-16 14:54:02 +0200

    Create ArrowConverter test

---
+++ app/src/test/java/casak/ru/geofencer/presentation/converters/ArrowConverterTest.java
@@ -0,0 +1,45 @@
+package casak.ru.geofencer.presentation.converters;
+
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import casak.ru.geofencer.domain.model.Arrow;
+import casak.ru.geofencer.domain.model.Point;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created on 16.02.2017.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ArrowConverterTest {
+
+    private Arrow arrow;
+    private List<Point> points;
+
+    @Before
+    public void setUpArrow() {
+        points = new ArrayList<>();
+        points.add(new Point());
+        points.add(new Point());
+
+        arrow = new Arrow(points, Arrow.Type.LEFT);
+    }
+
+    @Test
+    public void convertToPresentationModel_fromArrowModel_returnPolylineOptions() {
+        PolylineOptions result = ArrowConverter.convertToPresentationModel(arrow);
+
+        assertTrue(result.isClickable());
+        assertTrue(result.isGeodesic());
+        assertEquals(points.size(), result.getPoints().size());
+    }
+
+}
